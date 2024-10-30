5d9658b9a94de6dcf7bf16d856dbebc4f72ce71a
Sira4110 <vladsirak9@gmail.com>
2024-10-30 14:31:29 +0200

    Sira4110 створив структуру проекту

---
+++ js/task-4.js
@@ -0,0 +1,21 @@
+const form = document.querySelector('.login-form');
+const destroyButton = document.querySelector('.destroy-btn');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+  } else {
+    console.log({ email, password });
+    form.reset();
+  }
+});
+
+destroyButton.addEventListener('click', () => {
+  form.reset();
+  console.clear();
+});
